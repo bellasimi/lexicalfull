bd3246b06f4d026b02e0c8e4f9eb822c701fadc0
bellasimi <worksy1111@gmail.com>
2023-09-11 19:21:00 +0900

    chore: 불필요한 코드 삭제

--- src/components/LexicalEditor/index.tsx
+++ src/components/LexicalEditor/index.tsx
@@ -15,113 +15,33 @@ import { SettingsContext, useSettings } from "./context/SettingsContext";
 import { SharedAutocompleteContext } from "./context/SharedAutocompleteContext";
 import { SharedHistoryContext } from "./context/SharedHistoryContext";
 import Editor from "./Editor";
-import PlaygroundNodes from "./nodes/PlaygroundNodes";
 import { TableContext } from "./plugins/TablePlugin";
-import PlaygroundEditorTheme from "./themes/PlaygroundEditorTheme";
 
-function prepopulatedRichText() {
-  const root = $getRoot();
-  if (root.getFirstChild() === null) {
-    const heading = $createHeadingNode("h1");
-    heading.append($createTextNode("Welcome to the playground"));
-    root.append(heading);
-    const quote = $createQuoteNode();
-    quote.append(
-      $createTextNode(
-        `In case you were wondering what the black box at the bottom is – it's the debug view, showing the current state of the editor. ` +
-          `You can disable it by pressing on the settings control in the bottom-left of your screen and toggling the debug view setting.`
-      )
-    );
-    root.append(quote);
-    const paragraph = $createParagraphNode();
-    paragraph.append(
-      $createTextNode("The playground is a demo environment built with "),
-      $createTextNode("@lexical/react").toggleFormat("code"),
-      $createTextNode("."),
-      $createTextNode(" Try typing in "),
-      $createTextNode("some text").toggleFormat("bold"),
-      $createTextNode(" with "),
-      $createTextNode("different").toggleFormat("italic"),
-      $createTextNode(" formats.")
-    );
-    root.append(paragraph);
-    const paragraph2 = $createParagraphNode();
-    paragraph2.append(
-      $createTextNode(
-        "Make sure to check out the various plugins in the toolbar. You can also use #hashtags or @-mentions too!"
-      )
-    );
-    root.append(paragraph2);
-    const paragraph3 = $createParagraphNode();
-    paragraph3.append(
-      $createTextNode(`If you'd like to find out more about Lexical, you can:`)
-    );
-    root.append(paragraph3);
-    const list = $createListNode("bullet");
-    list.append(
-      $createListItemNode().append(
-        $createTextNode(`Visit the `),
-        $createLinkNode("https://lexical.dev/").append(
-          $createTextNode("Lexical website")
-        ),
-        $createTextNode(` for documentation and more information.`)
-      ),
-      $createListItemNode().append(
-        $createTextNode(`Check out the code on our `),
-        $createLinkNode("https://github.com/facebook/lexical").append(
-          $createTextNode("GitHub repository")
-        ),
-        $createTextNode(`.`)
-      ),
-      $createListItemNode().append(
-        $createTextNode(`Playground code can be found `),
-        $createLinkNode(
-          "https://github.com/facebook/lexical/tree/main/packages/lexical-playground"
-        ).append($createTextNode("here")),
-        $createTextNode(`.`)
-      ),
-      $createListItemNode().append(
-        $createTextNode(`Join our `),
-        $createLinkNode("https://discord.com/invite/KmG4wQnnD9").append(
-          $createTextNode("Discord Server")
-        ),
-        $createTextNode(` and chat with the team.`)
-      )
-    );
-    root.append(list);
-    const paragraph4 = $createParagraphNode();
-    paragraph4.append(
-      $createTextNode(
-        `Lastly, we're constantly adding cool new features to this playground. So make sure you check back here when you next get a chance :).`
-      )
-    );
-    root.append(paragraph4);
-  }
+interface Props {
+  readonly?: boolean;
+  initialValue?: string;
+  onValueChange: (value: string) => void;
+  config: any;
 }
 
-const LexicalEditor = (): JSX.Element => {
-  const {
-    settings: { isCollab, emptyEditor },
-  } = useSettings();
-
-  const initialConfig = {
-    editorState: prepopulatedRichText,
-    namespace: "Playground",
-    nodes: [...PlaygroundNodes],
-    onError: (error: Error) => {
-      throw error;
-    },
-    theme: PlaygroundEditorTheme,
-  };
-
+const LexicalEditor = ({
+  readonly,
+  initialValue,
+  onValueChange,
+  config,
+}: Props): JSX.Element => {
   return (
     <SettingsContext>
-      <LexicalComposer initialConfig={initialConfig}>
+      <LexicalComposer initialConfig={config}>
         <SharedHistoryContext>
           <TableContext>
             <SharedAutocompleteContext>
               <div className="editor-shell">
-                <Editor />
+                <Editor
+                  readonly={readonly}
+                  initialValue={initialValue}
+                  onValueChange={onValueChange}
+                />
               </div>
             </SharedAutocompleteContext>
           </TableContext>
@@ -132,3 +52,83 @@ const LexicalEditor = (): JSX.Element => {
 };
 
 export default LexicalEditor;
+
+// function prepopulatedRichText() {
+//   const root = $getRoot();
+//   if (root.getFirstChild() === null) {
+//     const heading = $createHeadingNode("h1");
+//     heading.append($createTextNode("Welcome to the playground"));
+//     root.append(heading);
+//     const quote = $createQuoteNode();
+//     quote.append(
+//       $createTextNode(
+//         `In case you were wondering what the black box at the bottom is – it's the debug view, showing the current state of the editor. ` +
+//           `You can disable it by pressing on the settings control in the bottom-left of your screen and toggling the debug view setting.`
+//       )
+//     );
+//     root.append(quote);
+//     const paragraph = $createParagraphNode();
+//     paragraph.append(
+//       $createTextNode("The playground is a demo environment built with "),
+//       $createTextNode("@lexical/react").toggleFormat("code"),
+//       $createTextNode("."),
+//       $createTextNode(" Try typing in "),
+//       $createTextNode("some text").toggleFormat("bold"),
+//       $createTextNode(" with "),
+//       $createTextNode("different").toggleFormat("italic"),
+//       $createTextNode(" formats.")
+//     );
+//     root.append(paragraph);
+//     const paragraph2 = $createParagraphNode();
+//     paragraph2.append(
+//       $createTextNode(
+//         "Make sure to check out the various plugins in the toolbar. You can also use #hashtags or @-mentions too!"
+//       )
+//     );
+//     root.append(paragraph2);
+//     const paragraph3 = $createParagraphNode();
+//     paragraph3.append(
+//       $createTextNode(`If you'd like to find out more about Lexical, you can:`)
+//     );
+//     root.append(paragraph3);
+//     const list = $createListNode("bullet");
+//     list.append(
+//       $createListItemNode().append(
+//         $createTextNode(`Visit the `),
+//         $createLinkNode("https://lexical.dev/").append(
+//           $createTextNode("Lexical website")
+//         ),
+//         $createTextNode(` for documentation and more information.`)
+//       ),
+//       $createListItemNode().append(
+//         $createTextNode(`Check out the code on our `),
+//         $createLinkNode("https://github.com/facebook/lexical").append(
+//           $createTextNode("GitHub repository")
+//         ),
+//         $createTextNode(`.`)
+//       ),
+//       $createListItemNode().append(
+//         $createTextNode(`Playground code can be found `),
+//         $createLinkNode(
+//           "https://github.com/facebook/lexical/tree/main/packages/lexical-playground"
+//         ).append($createTextNode("here")),
+//         $createTextNode(`.`)
+//       ),
+//       $createListItemNode().append(
+//         $createTextNode(`Join our `),
+//         $createLinkNode("https://discord.com/invite/KmG4wQnnD9").append(
+//           $createTextNode("Discord Server")
+//         ),
+//         $createTextNode(` and chat with the team.`)
+//       )
+//     );
+//     root.append(list);
+//     const paragraph4 = $createParagraphNode();
+//     paragraph4.append(
+//       $createTextNode(
+//         `Lastly, we're constantly adding cool new features to this playground. So make sure you check back here when you next get a chance :).`
+//       )
+//     );
+//     root.append(paragraph4);
+//   }
+// }
